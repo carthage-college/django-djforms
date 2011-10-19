918c406911cc10f60b8de840dcfef1291ea87588
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2011-10-19 20:37:59 +0000

    deployed

--- president/contact/forms.py
+++ president/contact/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    name        = forms.CharField(required=False)
-    email       = forms.EmailField()
+    name        = forms.CharField(label="Name (optional)", required=False)
+    email       = forms.EmailField(label="Email (optional)", required=False)
     comments    = forms.CharField(widget=forms.Textarea)
+
--- president/contact/views.py
+++ president/contact/views.py
@@ -13,14 +13,16 @@ def contact_form(request):
         if form.is_valid():
             cd = form.cleaned_data
             bcc = settings.MANAGERS
-            #to = ["presidentsearch@carthage.edu",]
-            to = ["skirk@carthage.edu",]
+            to = ["presidentsearch@carthage.edu",]
+            frm = cd.get("email")
+            if not frm:
+                frm = "confirmation@carthage.edu"
             t = loader.get_template('president/contact/email.html')
             c = RequestContext(request, {'data':cd,})
-            email = EmailMessage(("[Presidential Search Comments] %s" % cd["name"]), t.render(c), cd["email"], to, bcc, headers = {'Reply-To': cd["email"],'From': cd["email"]})
+            email = EmailMessage(("[Presidential Search Comments] %s" % cd["name"]), t.render(c), frm, to, bcc, headers = {'Reply-To': frm,'From': frm})
             email.content_subtype = "html"
             email.send(fail_silently=True)
-            return HttpResponseRedirect('/forms/president/contact/success')
+            return HttpResponseRedirect('/president-search/contact/success/')
     else:
         form = ContactForm()
     return render_to_response("president/contact/contact_form.html", {"form": form,'type':type}, context_instance=RequestContext(request))
