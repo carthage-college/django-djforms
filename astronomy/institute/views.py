a392b6dfa9eaf5328695650e3e392289ccede0c0
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2012-03-02 19:42:48 +0000

    going to production

--- astronomy/institute/views.py
+++ astronomy/institute/views.py
@@ -12,14 +12,14 @@ def night_report(request):
         if form.is_valid():
             data = form.cleaned_data
             bcc = settings.MANAGERS
-            recipient_list = ["larry@carthage.edu",]
-            #recipient_list = ["darion@carthage.edu",]
+            #recipient_list = ["larry@carthage.edu",]
+            recipient_list = ["darion@carthage.edu",]
             t = loader.get_template('astronomy/institute/night_report_email.html')
             c = RequestContext(request, {'object':data,})
-            email = EmailMessage(("[Griffin Observatory Night Report] Submitted by: %s of %s" % (object['name'],object['organization'])), t.render(c), object['email'], recipient_list, bcc, headers = {'Reply-To': object['email'],'From': object['email']})
+            email = EmailMessage(("[Griffin Observatory Night Report] Submitted by: %s of %s" % (data['name'],data['organization'])), t.render(c), data['email'], recipient_list, bcc, headers = {'Reply-To': data['email'],'From': data['email']})
             email.content_subtype = "html"
             email.send(fail_silently=True)
-            return HttpResponseRedirect('/forms/astronomy/institute/success/')
+            return HttpResponseRedirect('/forms/astronomy/institute/night-report/success/')
     else:
         form = NightReportForm()
     return render_to_response("astronomy/institute/night_report_form.html", {"form": form,}, context_instance=RequestContext(request))
