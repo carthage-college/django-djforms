f219f74d03a03236d8913daa12b266794cb75ad8
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2012-04-19 22:45:22 +0000

    weird

--- dining/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from djforms.dining.models import Event
-
-class EventAdmin(admin.ModelAdmin):
-    model = Event
-    ordering = ['created_on',]
-
-admin.site.register(Event, EventAdmin)
