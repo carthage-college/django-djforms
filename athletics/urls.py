dc76ae23dc58d086889ae046ff72997a5f82e0fb
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2012-05-02 16:12:00 +0000

    removed camp detail

--- athletics/urls.py
+++ athletics/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('djforms.athletics',
-    #url(r'^soccer/camp/(?P<rid>\d+)/detail/$', 'soccer.views.registration_detail', name="soccer_camp_detail"),
     url(r'^soccer/camp/success/$', direct_to_template, {'template': 'athletics/soccer/camp_registration_done.html'}, name="soccer_camp_success"),
     url(r'^soccer/camp/$', 'soccer.views.camp_registration', name='soccer_camp_registration'),
 )
