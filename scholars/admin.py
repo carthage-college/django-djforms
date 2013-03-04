883828595c5c77fd4885acae4424c08f8d7a9881
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2013-03-04 21:20:28 +0000

    going to psc

--- scholars/admin.py
+++ scholars/admin.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import admin
 from djforms.scholars.models import *
 from django.http import HttpResponse
@@ -9,9 +10,10 @@ def export_evs_requests(modeladmin, request, queryset):
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename=celebration_of_scholars.csv'
     writer = csv.writer(response)
-    writer.writerow(['ID', 'Title', 'First Name', 'Last Name', 'Email', 'Funding Source', 'Work Type', 'Permission to Reproduce', 'Faculty Sponsor Approval', 'Link', 'Abstract'])
+    writer.writerow(['Title', 'First Name', 'Last Name', 'Email', 'Funding Source', 'Work Type', 'Permission to Reproduce', 'Faculty Sponsor Approval', 'Link'])
     for p in queryset:
-        writer.writerow([p.id, p.title, p.leader.first_name, p.leader.last_name, p.user.email, p.funding, p.work_type, p.permission, p.shared, p.get_absolute_url(), p.abstract_text])
+        link = "http://%s%s" % (settings.SERVER_URL,p.get_absolute_url())
+        writer.writerow([p.title.encode('utf-8'), p.leader.first_name, p.leader.last_name, p.user.email, p.funding, p.work_type, p.permission, p.shared, link])
     return response
 export_evs_requests.short_description = "Export the selected Celebration of Scholars Submissions"
 
