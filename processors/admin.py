0ed884ea5493967f5cc588ce37f7d9228528902c
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2013-03-12 22:48:02 +0000

    e-looking glass admin updates

--- processors/admin.py
+++ processors/admin.py
@@ -4,10 +4,10 @@ from models import *
 class OrderAdmin(admin.ModelAdmin):
     model = Order
 
-    list_display  = ('last_name', 'contact_name', 'email', 'promotion', 'time_stamp', 'status', 'auth', 'cycle', 'payments', 'start_date','transid')
+    list_display  = ('last_name', 'contact_name', 'email', 'operator', 'promotion', 'time_stamp', 'status', 'auth', 'cycle', 'payments', 'start_date','transid')
     ordering      = ['promotion', '-time_stamp','status','auth','avs','cycle','payments','start_date']
     list_filter   = ('status','auth','avs','cycle','payments','promotion')
-    search_fields = ('email,', 'last_name', 'transid')
+    search_fields = ('transid')
     raw_id_fields = ('promotion',)
 
     def contact_name(self, obj):
