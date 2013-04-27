b7183b73d50a06a2ffc9f9555e8fcbdbeb3b9235
skirk <skirk@a79d55e6-e6c8-418a-94fb-64c884739af0>
2013-04-27 22:16:25 +0000

    final tweaks to classnotes

--- alumni/classnotes/forms.py
+++ alumni/classnotes/forms.py
@@ -14,7 +14,7 @@ CLASSYEARS.insert(0,("","Your class"))
 SPOUSEYEARS.insert(0,("","Spouse's class"))
 
 class ContactForm(forms.ModelForm):
-    classyear       = forms.CharField(label="Class", max_length=4, widget=forms.Select(choices=CLASSYEARS))
+    classyear       = forms.CharField(label="Class", max_length=4, widget=forms.Select(choices=CLASSYEARS, attrs={'class': 'required','required': 'required'}), required=True)
     spouseyear      = forms.CharField(label="Spouse's class", max_length=4, widget=forms.Select(choices=SPOUSEYEARS), required=False)
     email           = forms.EmailField(label="Email", required=False)
     classnote       = forms.CharField(widget=forms.Textarea, label="Your message to the Carthage community", required=True)
