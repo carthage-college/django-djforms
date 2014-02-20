e239406dfb746119a61f5a298caa19873a79727a
Bender <plungerman@gmail.com>
2014-02-20 10:54:26 -0600

    links between donation and pledge forms

--- djforms/giving/urls.py
+++ djforms/giving/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('djforms.giving.views',
-    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/success/(?P<campaign>[a-zA-Z0-9_-]+)/$', 'giving_success', name="giving_success"),
-    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/success/$', 'giving_success', name="giving_success"),
-    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/(?P<campaign>[a-zA-Z0-9_-]+)', 'giving_form', name='giving_form'),
-    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)', 'giving_form', name='giving_form'),
+    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/success/(?P<campaign>[a-zA-Z0-9_-]+)/$', 'giving_success', name="giving_success_campaign"),
+    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/success/$', 'giving_success', name="giving_success_generic"),
+    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/(?P<campaign>[a-zA-Z0-9_-]+)', 'giving_form', name='giving_form_campaign'),
+    url(r'^(?P<transaction>[a-zA-Z0-9_-]+)/', 'giving_form', name='giving_form_generic'),
 )
