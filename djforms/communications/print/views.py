67acf36f3b883f297dc95c59b6de18b91180c6ea
reZach <reZach@users.noreply.github.com>
2014-08-22 13:44:11 -0500

    Update views.py

--- djforms/communications/print/views.py
+++ djforms/communications/print/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.core.urlresolvers import reverse_lazy
 
-from djforms.communications.print.forms import ChanceOfForm
+from djforms.communications.print.forms import RequestForm
 from djtools.utils.mail import send_mail
 
