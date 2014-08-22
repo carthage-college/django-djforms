15ddfc5094c367f26a00ecefc31afe05c69033c9
reZach <reZach@users.noreply.github.com>
2014-08-22 13:43:53 -0500

    Update views.py

---
+++ djforms/communications/print/views.py
@@ -0,0 +1,9 @@
+from django.conf import settings
+from django.http import HttpResponseRedirect
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse_lazy
+
+from djforms.communications.print.forms import ChanceOfForm
+from djtools.utils.mail import send_mail
+
