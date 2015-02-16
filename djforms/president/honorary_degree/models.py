7ac4cd8cdaf0f3ed1c3e7c7838e694b2370e6d9b
Bender <plungerman@gmail.com>
2015-02-16 13:00:33 -0600

    moved upload_to_path to djtools

--- djforms/president/honorary_degree/models.py
+++ djforms/president/honorary_degree/models.py
@@ -2,22 +2,7 @@
 from django.db import models
 
 from djforms.core.models import GenericContact
-
-from uuid import uuid4
-
-from os.path import join
-
-def upload_to_path(self, filename):
-    """
-    Rename file to random string and generate path for a file field.
-    """
-    ext = filename.split('.')[-1]
-    # set filename as random string
-    filename = '{}.{}'.format(uuid4().hex, ext)
-    path = "files/{}".format(
-        self.get_slug()
-    )
-    return join(path, filename)
+from djtools.fields.helpers import upload_to_path
 
 
 class Nomination(GenericContact):
