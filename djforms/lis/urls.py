98a7d97140651dd2883f0cbe5f6420fd77515b93
Bender <plungerman@gmail.com>
2015-06-29 12:52:29 -0500

    forgot to remove the urls from lis collection

--- djforms/lis/urls.py
+++ djforms/lis/urls.py
@@ -13,26 +13,4 @@ urlpatterns = patterns('djforms.lis',
         TemplateView.as_view(template_name='lis/data_entered.html'),
         name="lis_success"
     ),
-    # course-ference
-    url(
-        r'^conferences/course-ference/map/json/$',
-        'conferences.course_ference.views.json_map_data',
-        name='course_ference_json_map_data'
-    ),
-    url(
-        r'^conferences/course-ference/map/$',
-        TemplateView.as_view(
-            template_name='lis/conferences/course_ference/map.html'
-        )
-    ),
-    url(
-        r'^conferences/course-ference/(?P<reg_type>[\d\w]+)/success/$',
-        'conferences.course_ference.views.registration_success',
-        name="course_ference_registration_success"
-    ),
-    url(
-        r'^conferences/course-ference/(?P<reg_type>[\d\w]+)/$',
-        'conferences.course_ference.views.registration_form',
-        name='course_ference_registration_form'
-    ),
 )
