f6427ce2ddbda2d4a479ba561f3721e93fa721de
Bender <plungerman@gmail.com>
2015-10-08 14:51:24 -0500

    forgot to make TO_LIST production value

--- djforms/global_bridge/__init__.py
+++ djforms/global_bridge/__init__.py
@@ -3,6 +3,5 @@ from django.conf import settings
 if settings.DEBUG:
     TO_LIST = [settings.SERVER_EMAIL,]
 else:
-    TO_LIST = [settings.SERVER_EMAIL,]
-    #TO_LIST = ["tkline@carthage.edu",]
+    TO_LIST = ["tkline@carthage.edu",]
 BCC = settings.MANAGERS
