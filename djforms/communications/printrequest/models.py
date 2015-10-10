a5859b2dac40186d9daea6f37957a01edc396260
Bender <plungerman@gmail.com>
2015-10-10 14:42:05 -0500

    removed unused 'choices' tuple from import

--- djforms/communications/printrequest/models.py
+++ djforms/communications/printrequest/models.py
@@ -3,8 +3,6 @@ from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
 
-from djtools.fields import BINARY_CHOICES
-
 FORMATS = (
     ('Booklet / Event program','Booklet / Event program'),
     ('Brochure','Brochure'),
