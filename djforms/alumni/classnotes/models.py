2b9563d1b2a2cf61514b52a6dbc709ff4c0c2752
Bender <plungerman@gmail.com>
2015-11-21 13:11:31 -0600

    Convert forms that use form.data to form.field_name.value in order to meet django 1.7 requirements. see https://www.wrike.com/open.htm?id=68956140

--- djforms/alumni/classnotes/models.py
+++ djforms/alumni/classnotes/models.py
@@ -1,16 +1,19 @@
 from django.db import models
 from django.conf import settings
+from django.core.urlresolvers import reverse
 
 from imagekit.models import ImageSpecField
 from imagekit.processors import ResizeToFill
 
 from djforms.core.models import GenericContact
 
-"""
-Model Contact
-"""
+from djtools.fields.helpers import upload_to_path
+
 
 class Contact(GenericContact):
+    """
+    Model Contact
+    """
     second_name = models.CharField(
         "Middle name",
         max_length=128, null=True, blank=True
@@ -77,7 +80,7 @@ class Contact(GenericContact):
     picture = models.ImageField(
         "Photo",
         max_length=255, blank=True, null=True,
-        upload_to="files/alumni/classnotes/photos",
+        upload_to=upload_to_path,
         help_text="75 dpi and .jpg only",
     )
     thumbnail = ImageSpecField(
@@ -97,13 +100,27 @@ class Contact(GenericContact):
         get_latest_by = 'created_at'
 
     def __unicode__(self):
-        return "%s, %s (%s)" % (self.last_name, self.first_name, self.classyear)
+        return "{}, {} ({})".format(
+            self.last_name, self.first_name, self.classyear
+        )
 
     def get_edit_url(self):
-        return "http://%s/forms/admin/classnotes/contact/%s/" % (settings.SERVER_URL, self.id)
+        """
+        return "http://{}/forms/admin/classnotes/contact/{}/".format(
+            settings.SERVER_URL, self.id
+        )
+        """
+        return reverse("admin:classnotes_contact_change", args=[self.id])
+
+
+    def get_slug(self):
+        return "files/alumni/classnotes/photos"
+
     def admin_image(self):
         if self.picture:
-            return '<a href="%s%s">Photo</a>' % (settings.MEDIA_URL,self.picture)
+            return '<a href="{}{}">Photo</a>'.format(
+                settings.MEDIA_URL,self.picture
+            )
         else:
             return None
     admin_image.allow_tags = True
