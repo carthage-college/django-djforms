9b52093def8d21a2dc4781066cd996a8e1d08d69
Bender <plungerman@gmail.com>
2015-11-25 10:06:18 -0600

    email distribution import from settings is now a list rather than a string we put into a list

--- djforms/polisci/iea/__init__.py
+++ djforms/polisci/iea/__init__.py
@@ -3,5 +3,5 @@ from django.conf import settings
 if settings.DEBUG:
     TO_LIST = [settings.SERVER_EMAIL,]
 else:
-    TO_LIST = [settings.POLITICAL_SCIENCE_IEA_EMAIL]
+    TO_LIST = settings.POLITICAL_SCIENCE_IEA_EMAIL
 BCC = settings.MANAGERS
