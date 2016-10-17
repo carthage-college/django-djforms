710c510dd6dbe05f73e10c23808c335cb99afac1
plungerman <plungerman@gmail.com>
2016-10-17 10:20:45 -0500

    updated the admin dashboard list display for copy/print card request form so that folks can see all fields

--- djforms/lis/copyprint/admin.py
+++ djforms/lis/copyprint/admin.py
@@ -5,9 +5,13 @@ class CardRequestAdmin(admin.ModelAdmin):
     model = CardRequest
     list_display  = (
         'last_name', 'first_name', 'email',
-        'entity_name','account_number','status'
+        'entity_name','account_number','entity_head',
+        'entity_treasurer','entity_advisor','printing_budget',
+        'date_created','date_updated','updated_by',
+        'status'
     )
-    raw_id_fields = ('user',)
+
+    raw_id_fields = ('user','updated_by')
     search_fields = ('last_name', 'email', 'entity_name')
 
     def save_model(self, request, obj, form, change):
