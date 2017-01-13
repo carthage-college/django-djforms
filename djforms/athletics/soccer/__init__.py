48023459ced555651c98c80c91f044b41409fe09
plungerman <plungerman@gmail.com>
2017-01-13 14:53:49 -0600

    new 'to' list for insurance card

--- djforms/athletics/soccer/__init__.py
+++ djforms/athletics/soccer/__init__.py
@@ -2,6 +2,9 @@ from django.conf import settings
 
 if settings.DEBUG:
     TO_LIST = [settings.SERVER_EMAIL,]
+    INSURANCE_TO_LIST = [settings.SERVER_EMAIL,]
 else:
     TO_LIST = settings.SOCCER_CAMP_TO_LIST
+    INSURANCE_TO_LIST = settings.SOCCER_CAMP_TO_LIST
+    INSURANCE_TO_LIST.append('soccercamprecords@carthage.edu')
 BCC = settings.MANAGERS
