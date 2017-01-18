805780227009921504f2aba96eda8f723c33fbce
plungerman <plungerman@gmail.com>
2017-01-18 12:51:43 -0600

    removed the 'authority' permissions stuff on the maintenance app, which appears to be unused, and which was causing a python error at the interactive prompt

--- djforms/maintenance/permissions.py
+++
@@ -1,9 +0,0 @@
-from authority import permissions, register
-from djforms.maintenance.models import MaintenanceRequest
-
-
-class MaintenanceRequestPermission(permissions.BasePermission):
-    label = 'maintenance_permission'
-    checks = ('review','update','delete',)
-
-register(MaintenanceRequest, MaintenanceRequestPermission)
