031263d2a769e80381d3a7669a57b1abfb9f52cf
plungerman <plungerman@gmail.com>
2017-01-30 16:55:35 -0600

    limit the permissions on UserProfile to GenericChoice items tagged 'Building name' and 'Maintenance Request Type'

--- djforms/core/admin.py
+++ djforms/core/admin.py
@@ -1,20 +1,53 @@
+from django import forms
 from django.contrib import admin
 from django.utils.translation import ugettext, ugettext_lazy as _
 from djforms.core.models import *
 
+from djforms.core.models import GenericChoice
+
+from tagging.models import Tag, TaggedItem
+
+mrt = Tag.objects.get(name__iexact='Maintenance Request Type')
+bld = Tag.objects.get(name__iexact='Building Name')
+
+PERMISSION = TaggedItem.objects.get_union_by_model(
+    GenericChoice, [mrt, bld]
+).filter(active=True).order_by("name")
+#except:
+#    PERMISSION = GenericChoice.objects.none()
+
+
+class UserProfileAdminForm(forms.ModelForm):
+
+  class Meta:
+    model = UserProfile
+
+  def __init__(self, *args, **kwargs):
+    super(UserProfileAdminForm, self).__init__(*args, **kwargs)
+    self.fields['permission'].queryset = PERMISSION
+
+
 class GenericChoiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'value', 'ranking', 'active', 'tags')
 
+
 class UserProfileAdmin(admin.ModelAdmin):
+    form = UserProfileAdminForm
+    filter_horizontal = ('permission',)
     raw_id_fields = ("user",)
-    search_fields = ('user__last_name','user__first_name','user__email','user__username',)
+    search_fields = (
+        'user__last_name','user__first_name','user__email','user__username',
+    )
+
 
 class PhotoAdmin(admin.ModelAdmin):
     pass
 
+
 class PromotionAdmin(admin.ModelAdmin):
     model = Promotion
     prepopulated_fields = {"slug": ("title",)}
+
 
 # core models
 admin.site.register(Department)
