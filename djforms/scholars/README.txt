bca415b8bea378a7ab7978c4b5b1d745a57b9756
plungerman <plungerman@gmail.com>
2017-04-17 17:31:20 -0500

    updates to shell scripts and documentation for celebration of scholars post-submission procedures

--- djforms/scholars/README.txt
+++ djforms/scholars/README.txt
@@ -1,3 +1,23 @@
-# Print
+# Various things to do after submissions have ended
 
-1. https://www.carthage.edu/forms/scholars/presentation/print/
+1. Print:
+
+https://www.carthage.edu/forms/scholars/presentation/print/
+
+2. Mugshots:
+
+python munge_mugshot.py
+
+will give you a list of files, which you can then use to tar:
+
+python tar_mugshots.py
+
+3. Posters:
+
+python munge_poster.py --test --year=xxxx > scholars.posters.txt
+
+execute tar with a file as the list of files for archiving:
+
+tar cvf posters.tar  --files-from=scholars.posters.txt
+
+4.
--- djforms/scholars/munge_poster.py
+++ djforms/scholars/munge_poster.py
@@ -1,22 +1,81 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#!/usr/bin/env
+
+import os
+import sys
+import uuid
+
+# env
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+sys.path.append('/usr/lib/python2.7/')
+sys.path.append('/usr/local/lib/python2.7/dist-packages/')
+sys.path.append('/data2/django_current/')
+sys.path.append('/data2/django_projects/')
+sys.path.append('/data2/django_third/')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djforms.settings')
+
+from django.conf import settings
 
 from djforms.scholars.models import Presentation
 
-import os, uuid
+import argparse
 
-prez = Presentation.objects.all()
+# set up command-line options
+desc = """
+Accepts as input the year to munge
+"""
 
-root_path = "/data2/django_projects/djforms/assets/"
-upload_dir = "files/scholars/posters/2013"
+parser = argparse.ArgumentParser(description=desc)
 
-for p in prez:
-    if p.poster_file:
-        orig         = root_path + p.poster_file.name
-        print "orig = %s" % orig
-        fname = p.poster_file.name.split('/')[-1]
-        new_filename = "%s/%s" % (upload_dir, fname)
-        new          = root_path + new_filename
-        print "new  %s" % new
-        os.rename(orig, new)
-        p.poster_file.name = new_filename
-        p.save()
+parser.add_argument(
+    '-y', '--year',
+    required=True,
+    help="Year to munge.",
+    dest='year'
+)
+parser.add_argument(
+    '--test',
+    action='store_true',
+    help="Dry run?",
+    dest='test'
+)
+
+def main():
+    '''
+    main function
+    '''
+
+    prez = Presentation.objects.filter(date_created__year=year)
+
+    root_path = settings.MEDIA_ROOT
+    upload_dir = '/files/scholars/posters/{}/'.format(year)
+
+    for p in prez:
+        if p.poster_file:
+            # original file
+            orig = '{}/{}'.format(root_path, p.poster_file.name)
+            # new file
+            fname = p.poster_file.name.split('/')[-1]
+            new_filename = '{}/{}'.format(upload_dir, fname)
+            new = root_path + new_filename
+            # rename and save
+            if not test:
+                os.rename(orig, new)
+                p.poster_file.name = new_filename
+                p.save()
+            else:
+                #print "orig = {}".format(orig)
+                print orig
+                #print "new  {}".format(new)
+
+
+######################
+# shell command line
+######################
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    year = args.year
+    test = args.test
+
+    sys.exit(main())
