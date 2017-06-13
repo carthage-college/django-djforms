dfe65d75366727ab987c0feff67b5f728ed94d59
plungerman <plungerman@gmail.com>
2017-06-13 11:06:45 -0500

    sql to convert django IDs to college IDs

--- djforms/bin/convert_userids.py
+++ djforms/bin/convert_userids.py
@@ -15,3 +15,37 @@ update core_userprofile set user_id =  where user_id =
 update auth_user_groups set user_id =  where user_id =
 update auth_user set id =  where id = 
 
+# celebration of scholars
+./scholars/models.py:    user = models.ForeignKey(
+./scholars/models.py:    updated_by = models.ForeignKey(
+# promotion
+./core/models.py:    user = models.ForeignKey(
+# catering
+./catering/models.py:    user = models.ForeignKey(
+# choral tryouts
+./music/ensembles/choral/models.py:    user = models.ForeignKey(
+# LIS print request
+./lis/copyprint/models.py:    user = models.ForeignKey(
+./lis/copyprint/models.py:    updated_by = models.ForeignKey(
+# writing curriculum
+./writingcurriculum/models.py:    user = models.ForeignKey(
+./writingcurriculum/models.py:    updated_by = models.ForeignKey(
+# applicant
+./prehealth/committee_letter/models.py:    created_by = models.ForeignKey(
+./prehealth/committee_letter/models.py:    updated_by = models.ForeignKey(
+# evaluation
+./prehealth/committee_letter/models.py:    created_by = models.ForeignKey(
+./prehealth/committee_letter/models.py:    updated_by = models.ForeignKey(
+# characterquest
+./characterquest/models.py:    profile = models.ForeignKey(UserProfile)
+# jobs
+./jobpost/models.py:    creator = models.ForeignKey(User, null=True, blank=True)
+# green pledge
+./sustainability/green/models.py:    user = models.ForeignKey(
+# maintenance
+./maintenance/models.py:    user = models.ForeignKey(
+./maintenance/models.py:    updated_by = models.ForeignKey(
+# print request
+./communications/printrequest/models.py:    user = models.ForeignKey(
+./communications/printrequest/models.py:    updated_by = models.ForeignKey(
+
