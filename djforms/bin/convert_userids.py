7ff214fec9ffcbf886b05533a5b89e94a6be4212
plungerman <plungerman@gmail.com>
2017-06-13 10:59:44 -0500

    sql to convert django IDs to college IDs

---
+++ djforms/bin/convert_userids.py
@@ -0,0 +1,17 @@
+
+update communications_printrequest set user_id =  where user_id =
+update communications_printrequest set updated_by_id =  where updated_by_id =
+
+update scholars_presentation set user_id =  where user_id =
+update scholars_presentation set updated_by_id=  where updated_by_id =
+
+update committee_letter_evaluation set created_by_id = where created_by_id =
+update committee_letter_evaluation set updated_by_id = where updated_by_id =
+
+update green_pledge set user_id =  where user_id =
+
+update core_userprofile set user_id =  where user_id = 
+
+update auth_user_groups set user_id =  where user_id =
+update auth_user set id =  where id = 
+
