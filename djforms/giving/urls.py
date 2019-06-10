cacb44997fffb6ec71a554e6797943ab719be1ed
plungerman <plungerman@gmail.com>
2019-06-10 14:12:17 -0500

    added some comments for documentation; removed trailing slash for campaigns

--- djforms/giving/urls.py
+++ djforms/giving/urls.py
@@ -5,6 +5,7 @@ from djforms.giving import views
 
 
 urlpatterns = [
+    # list of donors displayed
     url(
         r'^donors/(?P<slug>[a-zA-Z0-9_-]+)',
         views.donors, name='giving_donors_campaign'
@@ -12,6 +13,7 @@ urlpatterns = [
     url(
         r'^donors', views.donors, name='giving_donors'
     ),
+    # manager views
     url(
         r'^manager/cash/$',
         views.manager_cash, name='giving_manager_cash'
@@ -21,7 +23,6 @@ urlpatterns = [
         TemplateView.as_view(template_name='giving/manager/success.html'),
         name='giving_manager_success'
     ),
-    # dashboard home
     url(
         r'^manager/$',
         views.manager, name='giving_manager_home'
@@ -34,11 +35,12 @@ urlpatterns = [
         r'^manager/(?P<slug>[a-zA-Z0-9_-]+)/$',
         views.manager, name='giving_manager_home_campaign'
     ),
-    # campaign, mini-goal, crowd fund challenge, etc.
+    # ajax calls for campaign, mini-goal, crowd fund challenge, etc.
     url(
         r'^campaign/(?P<slug>[a-zA-Z0-9_-]+)',
         views.promotion_ajax, name='promotion_ajax'
     ),
+    # displayed after form is submitted
     url(
         r'^(?P<transaction>[a-zA-Z0-9_-]+)/(?P<campaign>[a-zA-Z0-9_-]+)/success',
         views.giving_success, name='giving_success_campaign'
@@ -47,10 +49,12 @@ urlpatterns = [
         r'^(?P<transaction>[a-zA-Z0-9_-]+)/success',
         views.giving_success, name='giving_success_generic'
     ),
+    # campaign donation forms
     url(
-        r'^(?P<transaction>[a-zA-Z0-9_-]+)/(?P<campaign>[a-zA-Z0-9_-]+)/',
+        r'^(?P<transaction>[a-zA-Z0-9_-]+)/(?P<campaign>[a-zA-Z0-9_-]+)',
         views.giving_form, name='giving_form_campaign'
     ),
+    # generic giving
     url(
         r'^(?P<transaction>[a-zA-Z0-9_-]+)',
         views.giving_form, name='giving_form_generic'
