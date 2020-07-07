4859300c8f0f817797dacba92ddde7e36ec874f1
plungerman <plungerman@gmail.com>
2020-07-07 08:41:37 -0500

    moved insurance distribution list to settings

--- djforms/athletics/soccer/__init__.py
+++ djforms/athletics/soccer/__init__.py
@@ -5,6 +5,5 @@ if settings.DEBUG:
     INSURANCE_TO_LIST = [settings.SERVER_EMAIL]
 else:
     TO_LIST = settings.SOCCER_CAMP_TO_LIST
-    INSURANCE_TO_LIST = settings.SOCCER_CAMP_TO_LIST
-    INSURANCE_TO_LIST.append('soccercamprecords@carthage.edu')
+    INSURANCE_TO_LIST = settings.INSURANCE_TO_LIST
 BCC = settings.MANAGERS
