874639f38305b470aad5d14577f0bff7e89b7185
plungerman <plungerman@gmail.com>
2021-09-27 09:31:56 -0500

    updates to code base for migration from python 2.7 to 3.8 and from django 1.11.x to django 2.2.24

--- djforms/lis/printjobs/views.py
+++ djforms/lis/printjobs/views.py
@@ -1,19 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import datetime
+
 from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.contrib.auth.decorators import login_required
-
 from djforms.lis.printjobs.forms import PrintRequestForm
-
 from djtools.utils.mail import send_mail
-
-import datetime
 
 
 @login_required
 def index(request):
-    if request.method=='POST':
+    if request.method == 'POST':
         form = PrintRequestForm(request.POST, request.FILES)
         if form.is_valid():
             data = form.cleaned_data
@@ -25,22 +25,22 @@ def index(request):
             else:
                 TO_LIST = [settings.LIS_PRINT_REQUEST_EMAIL, data['email']]
 
-            subject = "[Print Request]: {} from the {} Department".format(
-                data['name'],data['department']
+            subject = '[Print Request]: {0} from the {1} Department'.format(
+                data['name'], data['department'],
             )
 
             send_mail(
-                request, TO_LIST,
-                subject, data['email'],
-                'lis/printjobs/email.html', data, BCC, attach=True
+                request,
+                TO_LIST,
+                subject,
+                data['email'],
+                'lis/printjobs/email.html',
+                data,
+                BCC,
+                attach=True,
             )
-
-            return HttpResponseRedirect(
-                reverse_lazy('lis_success')
-            )
+            return HttpResponseRedirect(reverse_lazy('lis_success'))
     else:
         form = PrintRequestForm()
 
-    return render(
-        request, 'lis/printjobs/form.html', {'form': form,}
-    )
+    return render(request, 'lis/printjobs/form.html', {'form': form})
