7224386c66b1f8c912305a64b9e0c1ec46bb19c5
plungerman <plungerman@gmail.com>
2021-10-12 10:41:51 -0500

    final updates before going live to production

--- djforms/wsgi_default.py
+++ djforms/wsgi_default.py
@@ -9,8 +9,8 @@ from django.core.wsgi import get_wsgi_application
 
 
 # python
-sys.path.append('/d2/python_venv/3.6/djforms/lib/python3.6/')
-sys.path.append('/d2/python_venv/3.6/djforms/lib/python3.6/site-packages/')
+sys.path.append('/d2/python_venv/3.8/djforms/lib/python3.8/')
+sys.path.append('/d2/python_venv/3.8/djforms/lib/python3.8/site-packages/')
 # django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djforms.settings.staging')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
