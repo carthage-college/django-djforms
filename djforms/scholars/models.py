ebb9924734fb90e8071e3ca7eefd8113d9fda9d0
plungerman <plungerman@gmail.com>
2022-02-28 15:34:09 -0600

    replace mimetypevalidators after migtation commands

--- djforms/scholars/models.py
+++ djforms/scholars/models.py
@@ -19,7 +19,6 @@ from taggit.managers import TaggableManager
 
 
 FILE_VALIDATORS = [MimetypeValidator('image/png')]
-FILE_VALIDATORS = []
 WORK_TYPES = (
     ('SURE', 'SURE'),
     ('Senior thesis', 'Senior thesis'),
