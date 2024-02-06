515261c9a3bfeb2491790abd800a36f2ea9881b2
plungerman <plungerman@gmail.com>
2024-02-06 11:59:22 -0600

    removed old soccer camp forms

--- djforms/athletics/soccer/__init__.py
+++
@@ -1,9 +0,0 @@
-from django.conf import settings
-
-if settings.DEBUG:
-    TO_LIST = [settings.SERVER_EMAIL]
-    INSURANCE_TO_LIST = [settings.SERVER_EMAIL]
-else:
-    TO_LIST = settings.SOCCER_CAMP_TO_LIST
-    INSURANCE_TO_LIST = settings.INSURANCE_TO_LIST
-BCC = settings.MANAGERS
