b39d0db967f85dbf979c0f97f26a591b2044ca28
plungerman <plungerman@gmail.com>
2024-02-12 14:44:18 -0600

    updates for migration from python 3.8 to 3.10

--- djforms/wsgi_default.py
+++ djforms/wsgi_default.py
@@ -9,10 +9,10 @@ from django.core.wsgi import get_wsgi_application
 
 
 # python
-sys.path.append('/d2/python_venv/3.8/djforms/lib/python3.8/')
-sys.path.append('/d2/python_venv/3.8/djforms/lib/python3.8/site-packages/')
+sys.path.append('/d2/python_venv/3.10/djforms/lib/python3.10/')
+sys.path.append('/d2/python_venv/3.10/djforms/lib/python3.10/site-packages/')
 # django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djforms.settings.staging')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djforms.settings.shell')
 os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
 os.environ.setdefault('TZ', 'America/Chicago')
 # wsgi
